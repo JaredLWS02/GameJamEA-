7142edf4025b016702d79489519474e7da5e9c84
N0name1020 <chanpladin@gmail.com>
2026-02-05 17:25:19 +0800

    added sound to 2 and 4

--- GameJam26/Assets/CK/Script/Level2/Level2Button.cs
+++ GameJam26/Assets/CK/Script/Level2/Level2Button.cs
@@ -1,52 +1,65 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Level2Button : MonoBehaviour
 {
+    [Header("Scene Settings")]
     public string CorrectlevelName;
     public string WronglevelName;
     public bool CorrentButton = false;
-    private bool playerInside = false;
+
+    [Header("UI")]
     public GameObject UI;
+
+    [Header("Audio")]
+    public AudioClip CorrectSound;
+    public AudioClip WrongSound;
+    public AudioSource ButtonSound;
+
+    private bool playerInside = false;
+    private bool isLoading = false; // prevent spam
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             playerInside = true;
-            Debug.Log("Player entered");
             UI.SetActive(true);
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             playerInside = false;
             UI.SetActive(false);
-            
         }
     }
-
 
     void Update()
     {
-        if (playerInside && Input.GetKeyDown(KeyCode.E))
+        if (playerInside && !isLoading && Input.GetKeyDown(KeyCode.E))
         {
-            LoadLevel();
+            StartCoroutine(PlaySoundAndLoad());
         }
     }
 
-    void LoadLevel()
+    IEnumerator PlaySoundAndLoad()
     {
+        isLoading = true;
+
         if (CorrentButton)
         {
+            ButtonSound.PlayOneShot(CorrectSound);
+            yield return new WaitForSeconds(0.5f);
             SceneManager.LoadScene(CorrectlevelName);
-
         }
-        else if(!CorrentButton)
+        else
         {
+            ButtonSound.PlayOneShot(WrongSound);
+            yield return new WaitForSeconds(0.5f);
             SceneManager.LoadScene(WronglevelName);
         }
     }
