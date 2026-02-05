e19d5f4ac9a15653ff868d793e1f0245c06bcc21
EZPZ3292 <angkky1122@gmail.com>
2026-02-05 20:18:58 +0800

    Revert "Merge branch 'Julius' into Ang"
    This reverts commit 4265bd711e52f32c8051f327ad6fbadd552cefa6, reversing
    changes made to 05731b4e7aecabe835334ad4feae6a911f849de0.

--- GameJam26/Assets/Julius/ScriptJ/Player Movement.cs
+++ GameJam26/Assets/Julius/ScriptJ/Player Movement.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine.UIElements;
+
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 8f;
@@ -11,69 +11,66 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private bool isGrounded;
     private bool isFacingRight = true;
-    private AudioManager audioManager;
-    
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        audioManager = GameObject.FindGameObjectWithTag("audioManager").GetComponent<AudioManager>();
     }
-        void Update()
+
+    void Update()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        Flip(moveInput);
+
+        RaycastHit2D hit = Physics2D.Raycast(
+            groundCheck.position,
+            Vector2.down,
+            groundCheckDistance,
+            jumpableLayer
+        );
+
+        isGrounded = hit.collider != null;
+
+        animator.SetFloat("Speed", Mathf.Abs(moveInput));
+        animator.SetBool("isGrounded", isGrounded);
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            float moveInput = Input.GetAxisRaw("Horizontal");
-            rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
 
-            Flip(moveInput);
+    void Flip(float moveInput)
+    {
+        if (moveInput > 0 && !isFacingRight)
+        {
+            isFacingRight = true;
+            Vector3 scale = transform.localScale;
+            scale.x = Mathf.Abs(scale.x);
+            transform.localScale = scale;
+        }
+        else if (moveInput < 0 && isFacingRight)
+        {
+            isFacingRight = false;
+            Vector3 scale = transform.localScale;
+            scale.x = -Mathf.Abs(scale.x);
+            transform.localScale = scale;
+        }
+    }
 
-            RaycastHit2D hit = Physics2D.Raycast(
+    void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(
                 groundCheck.position,
-                Vector2.down,
-                groundCheckDistance,
-                jumpableLayer
+                groundCheck.position + Vector3.down * groundCheckDistance
             );
-
-            isGrounded = hit.collider != null;
-
-            animator.SetFloat("Speed", Mathf.Abs(moveInput));
-            animator.SetBool("isGrounded", isGrounded);
-
-
-            if (Input.GetButtonDown("Jump") && isGrounded)
-            {
-                audioManager.PlayPlayerSFX(audioManager.jumpSFX);
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            }
-        }
-
-        void Flip(float moveInput)
-        {
-            if (moveInput > 0 && !isFacingRight)
-            {
-                isFacingRight = true;
-                Vector3 scale = transform.localScale;
-                scale.x = Mathf.Abs(scale.x);
-                transform.localScale = scale;
-            }
-            else if (moveInput < 0 && isFacingRight)
-            {
-                isFacingRight = false;
-                Vector3 scale = transform.localScale;
-                scale.x = -Mathf.Abs(scale.x);
-                transform.localScale = scale;
-            }
-        }
-
-        void OnDrawGizmos()
-        {
-            if (groundCheck != null)
-            {
-                Gizmos.color = Color.red;
-                Gizmos.DrawLine(
-                    groundCheck.position,
-                    groundCheck.position + Vector3.down * groundCheckDistance
-                );
-            }
         }
     }
-
+}
--- GameJam26/Assets/Julius/ScriptJ/Vacuum Patrol.cs
+++ GameJam26/Assets/Julius/ScriptJ/Vacuum Patrol.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+
 
 using UnityEngine;
 
@@ -8,12 +8,11 @@ public class VacuumPatrol : MonoBehaviour
     public float moveSpeed = 20f;
     private int direction = 1; // 1 = right, -1 = left
     public LayerMask turnAroundLayer;
-    public int winCountdown = 5;
-    private AudioManager audioManager;
+
     void FixedUpdate()
     {
         transform.position += new Vector3(direction * moveSpeed * Time.deltaTime, 0, 0);
-        audioManager = GameObject.FindGameObjectWithTag("audioManager").GetComponent<AudioManager>();
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -27,41 +26,21 @@ public class VacuumPatrol : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")&&this.moveSpeed<=20)
+        if (collision.CompareTag("Player"))
         {
             collision.transform.parent = this.transform;
-            StartCoroutine(AttachPlayer(winCountdown));
-        }
-        else
-        {
-            return;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")&&collision.transform.parent == this.transform)
+        if (collision.CompareTag("Player"))
         {
-           
             collision.transform.parent = null;
-            StopAllCoroutines();
-            Debug.Log("stop coroutine");
         }
-        else
-        {
-            return;
-        }
-       
     }
-    IEnumerator AttachPlayer(int countdown)
-    {
-       
-        yield return new WaitForSeconds(winCountdown);
-        audioManager.pl
-        Debug.Log("Player Wins!");
-        
-    }
-    public void SpeedRandomize()
+
+   public void SpeedRandomize()
     {
         moveSpeed = Random.Range(1f, 40f);
     }
